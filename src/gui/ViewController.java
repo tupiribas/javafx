f60d584d304c8c423d969dc3c9a2c643a4ac9c34
Tupi Guedes <tupiguedes@outlook.com>
2021-08-25 11:39:57 -0300

    implementing calculator

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,30 +1,65 @@
 package gui;
 
-import gui.util.Alerts;
+import java.util.Locale;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
 	@FXML
-	private Button btTeste;
-	@FXML
-	private Label lbTeste;
+	private TextField txtNumber1;
 
 	@FXML
-	public void onbtTesteAction() {
-		if (lbTeste.getText() == "Olá JavaFX!") {
-			lbTeste.setText("Olá mundo!");
-		} 
-		else {
-			lbTeste.setText("Olá JavaFX!");
-		}
-	}
+	private TextField txtNumber2;
+
+	@FXML
+	private Label labelResult;
+
+	@FXML
+	private Button btSun;
 	
 	@FXML
-	public void onbtTestActionAlert() {
-		Alerts.showAlert("Alert!", null, "Bom dia sol!", AlertType.INFORMATION);
+	private Button btSub;
+	
+	@FXML
+	private Button btMult;
+
+	@FXML
+	private Button btDiv;
+	
+	public void calculateSum() {
+		Locale.setDefault(Locale.US);
+		double value1 = Double.parseDouble(txtNumber1.getText());
+		double value2 = Double.parseDouble(txtNumber2.getText());
+		double sum = value1 + value2;
+		labelResult.setText(String.format("%.2f", sum));;
 	}
+	
+	public void calculateSub() {
+		Locale.setDefault(Locale.US);
+		double value1 = Double.parseDouble(txtNumber1.getText());
+		double value2 = Double.parseDouble(txtNumber2.getText());
+		double sub = value1 - value2;
+		labelResult.setText(String.format("%.2f", sub));
+	}
+	
+	public void calculateMult() {
+		Locale.setDefault(Locale.US);
+		double value1 = Double.parseDouble(txtNumber1.getText());
+		double value2 = Double.parseDouble(txtNumber2.getText());
+		double mult = value1 * value2;
+		labelResult.setText(String.format("%.2f", mult));
+	}
+	
+	public void calculateDiv() {
+		Locale.setDefault(Locale.US);
+		double value1 = Double.parseDouble(txtNumber1.getText());
+		double value2 = Double.parseDouble(txtNumber2.getText());
+		double div = value1 / value2;
+		labelResult.setText(String.format("%.2f", div));
+	}
+
 }
