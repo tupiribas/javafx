6d95259bb516fe9ea12ba6a4e3766e064fe79d75
Tupi Guedes <tupiguedes@outlook.com>
2021-08-26 12:25:50 -0300

    comboBox implemented

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,112 +1,63 @@
 package gui;
 
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import gui.util.Alerts;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
 
 public class ViewController implements Initializable {
 
 	@FXML
-	private TextField txtNumber1;
+	private ComboBox<Person> comboBoxPerson;
+
+	private ObservableList<Person> obsListPerson;
 
 	@FXML
-	private TextField txtNumber2;
+	private Button btAll;
 
 	@FXML
-	private Label labelResult;
-	
-	@FXML 
-	public Label labelMensageError;
-
-	@FXML
-	private Button btSun;
-	
-	@FXML
-	private Button btSub;
-	
-	@FXML
-	private Button btMult;
-
-	@FXML
-	private Button btDiv;
-	
-	public void calculateSum() {
-		try {
-			Locale.setDefault(Locale.US);
-			double value1 = Double.parseDouble(txtNumber1.getText());
-			double value2 = Double.parseDouble(txtNumber2.getText());
-			double sum = value1 + value2;
-			labelResult.setText(String.format("%.2f", sum));
-		}
-		catch (NumberFormatException e) {
-			labelResult.setText("ERROR");
-			String[] i = e.getMessage().split(" ");
-			Alerts.showAlert("Error cod.:01", null, "Your mistake was: " + i[3] + 
-					"\nTry typing a number like: ... -3 or, -2 or, -1 or, 0 or, 1 or, 2 or, 3 or ..."
-					, AlertType.ERROR);           
+	public void onBtAllAction() {
+		for (Person person : obsListPerson) {
+			System.out.println(person);
 		}
 	}
-	
-	public void calculateSub() {
-		try {
-			Locale.setDefault(Locale.US);
-			double value1 = Double.parseDouble(txtNumber1.getText());
-			double value2 = Double.parseDouble(txtNumber2.getText());
-			double sub = value1 - value2;
-			labelResult.setText(String.format("%.2f", sub));
-		}
-		catch (NumberFormatException e) {
-			labelResult.setText("ERROR");
-			Alerts.showAlert("Error cod.:02", null, "Try typing a number like: -3, -2, -1, 0, 1, 2, 3"
-					, AlertType.ERROR);
-		}
-	}
-	
-	public void calculateMult() {
-		try {
-			Locale.setDefault(Locale.US);
-			double value1 = Double.parseDouble(txtNumber1.getText());
-			double value2 = Double.parseDouble(txtNumber2.getText());
-			double mult = value1 * value2;
-			labelResult.setText(String.format("%.2f", mult));
-		}
-		catch (NumberFormatException e) {
-			labelResult.setText("ERROR");
-			Alerts.showAlert("Error cod.:03", null, "Try typing a number like: -3, -2, -1, 0, 1, 2, 3"
-					, AlertType.ERROR);
-		}
-	}
-	
-	public void calculateDiv() {
-		try {
-			Locale.setDefault(Locale.US);
-			double value1 = Double.parseDouble(txtNumber1.getText());
-			double value2 = Double.parseDouble(txtNumber2.getText());
-			double div = value1 / value2;
-			labelResult.setText(String.format("%.2f", div));
-		}
-		catch (NumberFormatException e) {
-			labelResult.setText("ERROR");
-			Alerts.showAlert("Error cod.:04", null, "Try typing a number like: -3, -2, -1, 0, 1, 2, 3"
-					, AlertType.ERROR);
-		}
+
+	@FXML
+	public void onComboBoxPersonalization() {
+		System.out.println(comboBoxPerson.getValue());
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		Constraints.setTextFieldDouble(txtNumber1);
-		Constraints.setTextFieldDouble(txtNumber2);
-		Constraints.setFieldMaxLength(txtNumber1, 12);
-		Constraints.setFieldMaxLength(txtNumber2, 12);
-		
+		List<Person> listPerson = new ArrayList<>();
+		listPerson.add(new Person(1, "Tupi", "tupi@hotmail.com"));
+		listPerson.add(new Person(2, "Lara Guedes Ribas", "laraguedes@gmail.com"));
+		listPerson.add(new Person(3, "Edânia Guedes Ribas", "edivaniaRi@outlook.com"));
+		listPerson.add(new Person(4, "Tupiraci de Andrade Ribas", "tupiracyRibas@hotmail.com"));
+		obsListPerson = FXCollections.observableArrayList(listPerson);
+		comboBoxPerson.setItems(obsListPerson);
+
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			};
+		};
+		comboBoxPerson.setCellFactory(factory);
+		comboBoxPerson.setButtonCell(factory.call(null));
+
 	}
 
 }
